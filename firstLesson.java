5768d1210f16ed085f4726ed2810f3b596f51d71
Ilias Badretdinov <ilyasbadrik@mail.ru>
2022-12-08 16:10:41 +0300

    My first lesson of java

---
+++ firstLesson.java
@@ -0,0 +1,7 @@
+public class firstLesson {
+    public static void main(String[] args) {
+        System.out.println("     +\n    +++\n   +++++\n  +++++++\n +++++++++\n    +++\n    +++");
+        System.out.println(2 + 2 * 2);
+        System.out.println("Hello, World!");
+    }
+}
