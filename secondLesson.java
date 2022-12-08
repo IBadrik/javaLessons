4466713256c755b76bfc4061f859889ed7049267
Ilias Badretdinov <ilyasbadrik@mail.ru>
2022-12-08 16:32:07 +0300

    This lesson about variables

---
+++ secondLesson.java
@@ -0,0 +1,11 @@
+public class secondLesson {
+    public static void main(String[] args) {
+        String myName = "Ilias";
+        String theLanguage = "Java";
+        System.out.println("Hello, my name is " + myName + " and I learn a programming language " + theLanguage);
+    }
+}
+
+/* This lesson about variables
+* Names of my variables must be in "CamelCase", variable "myName" is not variable "MyNAME"
+* Not valid names of variables: my-name, the%language, 4myName, */
