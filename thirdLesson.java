8db6f264ab4caa3efa642e8324c0a02cae264ac8
Ilias Badretdinov <ilyasbadrik@mail.ru>
2022-12-09 15:18:49 +0300

    This lesson about types

---
+++ thirdLesson.java
@@ -0,0 +1,12 @@
+public class thirdLesson {
+    public static void main(String[] args) {
+        char symbol = 't';
+        System.out.println(symbol);
+        System.out.println((int)symbol);
+        char firstPartOfWord = 37325;
+        char secondPartOfWord = 21147;
+        System.out.println("The gravity in Chinese - " + firstPartOfWord + secondPartOfWord);
+    }
+}
+
+/* This lesson about types*  = char, byte, short, int, long, double, float, boolean  */
