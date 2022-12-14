3ecbe1bb3b61172d8f30391e900ebc99e476af8e
Ilias Badretdinov <ilyasbadrik@mail.ru>
2022-12-14 18:52:52 +0300

    This lesson about type booleand and operator 'if'

---
+++ sixthLesson.java
@@ -0,0 +1,14 @@
+public class sixthLesson {
+    public static void main(String[] args) {
+        int temperature = -5;
+        if (temperature > 0) {
+            System.out.println("nice weather!");
+        } else if (temperature < 0) {
+            System.out.println("cold");
+        } else {
+            System.out.println("not nice and not cold");
+        }
+    }
+}
+
+/* This lesson about type boolean and operator "if" */
