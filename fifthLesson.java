e4383e9da0a4cdc9e155f007121b0cedf2068f78
Ilias Badretdinov <ilyasbadrik@mail.ru>
2022-12-14 14:55:37 +0300

    This lesson about types float and double

---
+++ fifthLesson.java
@@ -0,0 +1,11 @@
+public class fifthLesson {
+    public static void main(String[] args) {
+        float numOne = 1000000F;
+        System.out.println(numOne / 3); // => 333333.34
+
+        double numTwo = 1000000.0;
+        System.out.println(numTwo / 3); // => 333333.3333333333
+    }
+}
+
+/* This lesson about types - float and double */
